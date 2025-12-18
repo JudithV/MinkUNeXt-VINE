763583537019e647a608dee25b4fb6299df66890
Judith Vilella-Cantos <42940890+JudithV@users.noreply.github.com>
2025-12-18 13:20:36 +0100

    Integrate MatryoshkaLoss into trainer.py
    Adapted the training script to include MatryoshkaLoss for improved loss calculation and adjusted the backward pass for gradient injection.

--- training/trainer.py
+++ training/trainer.py
@@ -1,7 +1,8 @@
-# Miguel Hernández University of Elche
+# Code adapted from MinkLoc3Dv2 repo: https://github.com/jac99/MinkLoc3Dv2.git
 # Institute for Engineering Research of Elche (I3E)
 # Automation, Robotics and Computer Vision lab (ARCV)
 # Author: Judith Vilella Cantos
+
 import os
 import numpy as np
 import torch
@@ -10,6 +11,7 @@ import tqdm
 import pathlib
 import wandb
 from losses.contrastive_loss import BatchHardContrastiveLossWithMasks
+from losses.matryoshka import MatryoshkaLoss
 from datasets.dataset_utils import make_dataloaders
 from pnv_evaluate import evaluate, print_eval_stats, pnv_write_eval_stats
 from pytorch_metric_learning.distances import LpDistance, CosineSimilarity
@@ -54,7 +56,7 @@ def training_step(global_iter, model, phase, device, optimizer, loss_fn):
     optimizer.zero_grad()
     distance = LpDistance(normalize_embeddings=PARAMS.normalize_embeddings)
     loss_fn_cluster = BatchHardContrastiveLossWithMasks(pos_margin=PARAMS.pos_margin, neg_margin=PARAMS.neg_margin, distance=distance)
-
+    loss_fn_matryoshka = MatryoshkaLoss(loss_fn, dims=[64, 128, 192], weights=[1.0, 0.5, 0.25])
     with torch.set_grad_enabled(phase == 'train'):
         y = model(batch)
 
@@ -62,12 +64,13 @@ def training_step(global_iter, model, phase, device, optimizer, loss_fn):
 
         embeddings = y['global']
 
-        loss, temp_stats = loss_fn(embeddings, positives_mask, negatives_mask)
+        #loss, temp_stats = loss_fn(embeddings, positives_mask, negatives_mask)
+        loss, temp_stats = loss_fn_matryoshka(embeddings, positives_mask, negatives_mask)
         #loss, temp_stats = loss_fn_cluster(embeddings, positives_mask, negatives_mask)
         if PARAMS.clustering_head:
             distance = LpDistance(normalize_embeddings=PARAMS.normalize_embeddings)
             loss_fn_cluster = BatchHardContrastiveLossWithMasks(pos_margin=PARAMS.pos_margin, neg_margin=PARAMS.neg_margin, distance=distance)
-            clustering = y['clustering_emb']
+            clustering = y['clustering_emb'] # y['clustering_emb']
             if clustering.shape[0] > PARAMS.cluster_batch_size:
                 idx = torch.randperm(clustering.shape[0])[:PARAMS.cluster_batch_size]
                 clustering_sub = clustering[idx]
@@ -81,7 +84,6 @@ def training_step(global_iter, model, phase, device, optimizer, loss_fn):
 
             loss_clust, _ = loss_fn(clustering, positives_mask, negatives_mask) # loss_fn_cluster
             lambda_clust = PARAMS.clustering_importance
-
             loss = loss + lambda_clust * loss_clust
         temp_stats = tensors_to_numbers(temp_stats)
         stats.update(temp_stats)
@@ -105,7 +107,7 @@ def multistaged_training_step(global_iter, model, phase, device, optimizer, loss
     batch, positives_mask, negatives_mask = next(global_iter)
     distance = LpDistance(normalize_embeddings=PARAMS.normalize_embeddings)
     loss_fn_cluster = BatchHardContrastiveLossWithMasks(pos_margin=PARAMS.pos_margin, neg_margin=PARAMS.neg_margin, distance=distance)
-
+    loss_fn_matryoshka = MatryoshkaLoss(loss_fn, dims=[64, 128, 192], weights=[1.0, 0.5, 0.25])
     if phase == 'train':
         model.train()
     else:
@@ -123,15 +125,15 @@ def multistaged_training_step(global_iter, model, phase, device, optimizer, loss
             y = model(minibatch)
             embeddings_l.append(y['global'])
             if PARAMS.clustering_head:
-                clustering_l.append(y['clustering_emb'])
+                clustering_l.append(y['clustering_emb']) # y['clustering_emb']
 
     torch.cuda.empty_cache()  # Prevent excessive GPU memory consumption by SparseTensors
 
-    # Stage 2 - compute gradient of the loss w.r.t embeddings
     embeddings = torch.cat(embeddings_l, dim=0)
     
     embeddings_grad = None
     clustering_grad = None
+    clustering_logits_grad = None
 
     if PARAMS.clustering_head:
         clustering = torch.cat(clustering_l, dim=0)
@@ -142,29 +144,15 @@ def multistaged_training_step(global_iter, model, phase, device, optimizer, loss
             if PARAMS.clustering_head:
                 clustering.requires_grad_(True)
 
-        # 1. Global loss
-        loss, stats = loss_fn(embeddings, positives_mask, negatives_mask)
+        loss, stats = loss_fn_matryoshka(embeddings, positives_mask, negatives_mask)
         stats = tensors_to_numbers(stats)
 
-        # 2. Additional head loss
         total_loss = loss
         
         if PARAMS.clustering_head:
-            
-            if clustering.shape[0] > PARAMS.cluster_batch_size:
-                idx = torch.randperm(clustering.shape[0])[:PARAMS.cluster_batch_size]
-                clustering_sub = clustering[idx]
-                positives_mask_sub = positives_mask[idx][:, idx]
-                negatives_mask_sub = negatives_mask[idx][:, idx]
-                
-                loss_clust, _ = loss_fn(clustering_sub, positives_mask_sub, negatives_mask_sub)
-                
-            else:
-                loss_clust, _ = loss_fn(clustering, positives_mask, negatives_mask)
-            
-            total_loss = total_loss + (PARAMS.clustering_importance * loss_clust)
+            loss_clust, _ = loss_fn(clustering, positives_mask, negatives_mask)
+            total_loss += (PARAMS.clustering_importance * loss_clust)
 
-        # Backward único para obtener gradientes de los EMBEDDINGS (no de la red aún)
         if phase == 'train':
             total_loss.backward()
             
@@ -172,12 +160,11 @@ def multistaged_training_step(global_iter, model, phase, device, optimizer, loss
             if PARAMS.clustering_head:
                 clustering_grad = clustering.grad
 
-    # Limpieza
     embeddings_l, embeddings, loss = None, None, None
     if PARAMS.clustering_head:
         clustering_l, clustering = None, None
 
-    # ---------------- Stage 3: Backpropagation a través del Backbone ----------------
+    # ---------------- Stage 3: Backpropagation ----------------
     if phase == 'train':
         optimizer.zero_grad()
         i = 0
@@ -190,26 +177,21 @@ def multistaged_training_step(global_iter, model, phase, device, optimizer, loss
                 embeddings_mb = y['global']
                 minibatch_size = embeddings_mb.shape[0]
                 
-                # Inyectar gradiente Global
                 if embeddings_grad is not None:
-                    # retain_graph=True es necesario porque vamos a hacer backward sobre la misma gráfica
-                    # para el clustering head justo después.
-                    embeddings_mb.backward(gradient=embeddings_grad[i: i+minibatch_size], retain_graph=PARAMS.clustering_head)
+                    retain_graph = PARAMS.clustering_head
+                    embeddings_mb.backward(gradient=embeddings_grad[i: i+minibatch_size], retain_graph=retain_graph)
 
-                # Inyectar gradiente Clustering
                 if PARAMS.clustering_head:
-                    clustering_mb = y['clustering_emb']
+                    clustering_mb = y['clustering_emb'] # y['clustering_emb']
                     
                     if clustering_grad is not None:
-                        # El gradiente ya viene escalado por lambda desde el Stage 2
-                        clustering_mb.backward(gradient=clustering_grad[i: i+minibatch_size], retain_graph=PARAMS.use_cross_entropy)
+                        clustering_mb.backward(gradient=clustering_grad[i: i+minibatch_size], retain_graph=PARAMS.clustering_head)
 
                 i += minibatch_size
 
             optimizer.step()
 
     torch.cuda.empty_cache()
-    # Retorna memoria reservada, etc.
     return stats, 0, 0
 
 
