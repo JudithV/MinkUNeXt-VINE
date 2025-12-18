69645de09f6cf3ecb747243687986c8eed90312c
Judith Vilella-Cantos <42940890+JudithV@users.noreply.github.com>
2025-12-18 13:17:48 +0100

    Remove unused constant from config

--- config/config.py
+++ config/config.py
@@ -54,7 +54,6 @@ class Config():
             self.neg_margin = config.get('neg_margin')
             self.normalize_embeddings = config.get('normalize_embeddings')
 
-            self.aggregator_fusion = config.get('aggregator_fusion')
             self.clustering_head = config.get('clustering_head')
             self.clustering_importance = float(config.get('clustering_importance'))
             self.cluster_batch_size = config.get('cluster_batch_size')
